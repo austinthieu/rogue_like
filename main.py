dbc77587a4fa3e608c5fa20da18d17990dc14c0c
Cynchronized <austin.thieu123@gmail.com>
2025-03-24 22:10:42 -0600

    Draw player on screen and move

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from __future__ import annotations
 
+import attrs
 import tcod.console
 import tcod.context
 import tcod.event
 import tcod.tileset
 
 
-# TODO: Move player
+@attrs.define()
+class ExampleState:
+    player_x: int
+    player_y: int
+
+    def on_draw(self, console: tcod.console.Console) -> None:
+        console.print(self.player_x, self.player_y, "@")
+
+    def on_event(self, event: tcod.event.Event) -> None:
+        match event:
+            case tcod.event.Quit():
+                raise SystemExit
+            case tcod.event.KeyDown(sym=tcod.event.KeySym.LEFT):
+                self.player_x -= 1
+            case tcod.event.KeyDown(sym=tcod.event.KeySym.RIGHT):
+                self.player_x += 1
+            case tcod.event.KeyDown(sym=tcod.event.KeySym.UP):
+                self.player_y -= 1
+            case tcod.event.KeyDown(sym=tcod.event.KeySym.DOWN):
+                self.player_y += 1
+
+
 def main() -> None:
+    # Run ExampleState
     tileset = tcod.tileset.load_tilesheet(
         "data/Alloy_curses_12x12.png",
         columns=16,
@@ -16,14 +39,15 @@ def main() -> None:
     )
     tcod.tileset.procedural_block_elements(tileset=tileset)
     console = tcod.console.Console(80, 50)
-    console.print(0, 0, "Hello World")
+    state = ExampleState(player_x=console.width // 2, player_y=console.height // 2)
     with tcod.context.new(console=console, tileset=tileset) as context:
         while True:
+            console.clear()
+            state.on_draw(console)
             context.present(console)
             for event in tcod.event.wait():
                 print(event)
-                if isinstance(event, tcod.event.Quit):
-                    raise SystemExit
+                state.on_event(event)  # Pass events to the state
 
 
 if __name__ == "__main__":
